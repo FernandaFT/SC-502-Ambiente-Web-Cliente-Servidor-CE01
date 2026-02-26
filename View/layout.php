96b82afdbf2ead048d9e09b919dd0fc5d999d5f0
Fernanda Fajardo <nanda199784@gmail.com>
2026-02-25 19:51:31 -0600

    actualización de titulo

--- View/layout.php
+++ View/layout.php
@@ -6,7 +6,7 @@ function MostrarCSS()
         '<head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-            <title>Fernanda Fajardo T02</title>
+            <title>Fernanda Fajardo CE01</title>
 
             <link rel="stylesheet" href="../assets/vendors/mdi/css/materialdesignicons.min.css">
             <link rel="stylesheet" href="../assets/vendors/ti-icons/css/themify-icons.css">
